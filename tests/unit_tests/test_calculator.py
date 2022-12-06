01d530254b35a8d97e58e81189c37cfdaea781af
ivanvikvik <ivanvikvik@gmail.com>
2022-12-06 20:13:19 +0300

    Using unit-testing on dynamic Calculator with custom method init() and destroy()

--- tests/unit_tests/test_calculator.py
+++ tests/unit_tests/test_calculator.py
@@ -4,57 +4,68 @@ from model.calculator import Calculator
 
 
 class TestCalculator(unittest.TestCase):
+
+    def init(self, a, b):
+        print("init")
+        self.calc = Calculator(a, b)
+
+    def destroy(self):
+        print("destroy")
+        del self.calc
+
     # AAA
     def test_sum(self):
         # arrange
         a = 10
         b = 20
+        self.init(a, b)
         expected = 30
 
         # action
-        actual = Calculator.sum(a, b)
+        actual = self.calc.sum()
 
         # assert
-        assert expected == actual, f"expected {expected}, but was {actual}"
+        self.assertEqual(expected, actual)
+        self.assertEqual(1, self.calc.count)
+
+        self.destroy()
 
     def test_sub(self):
-        # arrange
         a = 10
         b = 7
+        self.init(a, b)
         expected = 3
 
-        # action
-        actual = Calculator.sub(a, b)
+        actual = self.calc.sub()
 
-        # assert
-        if expected != actual:
-            self.fail()
+        self.assertEqual(expected, actual)
+        self.assertEqual(1, self.calc.count)
+        self.destroy()
 
     def test_mul(self):
-        # arrange
         a = 8
         b = 7
+        self.init(a, b)
         expected = 56
 
-        # action
-        actual = Calculator.mul(a, b)
+        actual = self.calc.mul()
 
-        # assert
         self.assertEqual(expected, actual)
+        self.assertEqual(1, self.calc.count)
+        self.destroy()
 
     def test_div(self):
-        # arrange
         a = 18
         b = 7
+        self.init(a, b)
         expected = 2
 
-        # action
-        actual = Calculator.div(a, b)
+        actual = self.calc.div()
 
-        # assert
         self.assertEqual(expected, actual)
+        self.assertEqual(1, self.calc.count)
+        self.destroy()
 
 
 if __name__ == "__main__":
     unittest.main()
-
